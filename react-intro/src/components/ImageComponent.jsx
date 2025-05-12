22800ccfc9e873c841b0c837b8839f661f5dc2a6
iTino7 <Sabatino.b007@outlook.it>
2025-05-12 16:49:33 +0200

    fixed project

--- react-intro/src/components/ImageComponent.jsx
+++ react-intro/src/components/ImageComponent.jsx
@@ -3,7 +3,20 @@ import { Component } from "react";
 class ImageComponent extends Component {
   render() {
     return (
-      <img src={this.props.img} className="card-img-top" alt={this.props.alt} />
+      <div className="card mt-3" style={{ width: "18rem" }}>
+        <img
+          src={this.props.img}
+          className="card-img-top"
+          alt={this.props.alt}
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card’s content.
+          </p>
+        </div>
+      </div>
     );
   }
 }
--- react-intro/src/components/Main.jsx
+++ react-intro/src/components/Main.jsx
@@ -1,10 +1,31 @@
-import Card from "./Card";
+import ButtonComponent from "./ButtonComponent";
+import ImageComponent from "./ImageComponent";
 
 function Main() {
   return (
     <div className="container-fluid d-flex justify-content-center">
       <div className="row ">
-        <Card />
+        <div className="col-12 col-sm-6 col-md-4 col-lg-4 align-items-center d-flex flex-column">
+          <ImageComponent img="https://www.autoscout24.it/cms-content-assets/7D9mKZlCtwKo06tNq8KZTE-623552c2c6e561cf0b32e47c3fefdf46-Mercedes-A-Klasse-2023-AMG-Hero-1100.jpg" />
+          <ButtonComponent
+            className="d-flex flex-column btn btn-success"
+            title="Scopri di più"
+          />
+        </div>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-4 align-items-center d-flex flex-column">
+          <ImageComponent img="https://www.autoscout24.it/cms-content-assets/3EiC4oHLrxbuGxHCtppxRs-1a7ee5cda36ff38b2f7930178ab6605e-BMW-740d-xDrive-Titelbild-1100.jpg" />
+          <ButtonComponent
+            className="d-flex flex-column btn btn-danger"
+            title="Elimina"
+          />
+        </div>
+        <div className="col-12 col-sm-6 col-md-4 col-lg-4 align-items-center d-flex flex-column">
+          <ImageComponent img="https://cdnwp.dealerk.com/ea42991a/uploads/sites/3/2024/01/k4a9728ferrari_v3-1-1024x683.jpg" />
+          <ButtonComponent
+            className="d-flex flex-column btn btn-warning"
+            title="Modifica"
+          />
+        </div>
       </div>
     </div>
   );
